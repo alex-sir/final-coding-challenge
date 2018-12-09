c0ffb28d0dd151678890fac42b1df982029861c9
alex-sir <44764536+alex-sir@users.noreply.github.com>
2018-12-08 23:35:23 -0800

    Add files via upload

--- src/collections/reduce.js
+++ src/collections/reduce.js
@@ -11,3 +11,45 @@
  * The result of invoking the callback is stored, and used for the next iteration.
  * Once all iterations are complete, reduce returns the final result.
  */
+
+// Includes objects and strings workings (I think)
+function reduce(ray, cb, initialValue) {
+
+    if (ray === null) {
+        throw new Error('Collection is null');
+    }
+
+    if (typeof ray === 'string' ||
+        typeof ray === 'object') {
+        var value;
+
+        if (typeof ray === 'string') {
+            for (var i = 0; i < ray.length; i++) {
+                value = cb.call(ray, initialValue, ray.charAt(i));
+                initialValue = value;
+            }
+            return value;
+        }
+
+        if (ray instanceof Array) {
+            for (var i = 0; i < ray.length; i++) {
+                value = cb.call(ray, initialValue, ray[i]);
+                initialValue = value;
+            }
+            return value;
+        }
+
+        if (typeof ray === 'object' && ray instanceof Array === false) {
+            for (var property in ray) {
+                value = cb.call(ray, initialValue, ray[property]);
+                initialValue = value;
+            }
+            return value;
+        }
+
+    } else {
+        throw new Error('Invalid data type');
+    }
+}
+
+module.exports = reduce;
